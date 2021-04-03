9bc056950bee01dc6b22a37b6fff2f96489c158a
star-lord-97 <a_mahmoud_1997@yahoo.com>
2021-04-03 14:01:35 +0200

    extracted helper functions and moved to a separate file

---
+++ helpers.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * goBack30Days:
+ * helper function that returns the date of the
+ * day -30 days from current day in the required format
+ *
+ * @return string
+ */
+function goBack30Days(): string
+{
+    $date = new DateTime();
+    $date = $date->sub(new DateInterval('P30D'));
+    return $date->format('Y-m-d');
+}
+
+/**
+ * formatURI:
+ * helper function that returns the uri with the
+ * correct date to use in the request
+ *
+ * @return string
+ */
+function formatURI(): string
+{
+    $date = goBack30Days();
+    return 'https://api.github.com/search/repositories?q=created:>' . $date . '&sort=stars&order=desc&per_page=100';
+}
+
+
+/**
+ * decodeAndRemoveMetadata:
+ * helper function that receives json string,
+ * converts it into an associative array and
+ * removes the metadata then returns it
+ *
+ * @param  mixed $jsonData
+ * @return array
+ */
+function decodeAndRemoveMetadata($jsonData): array
+{
+    $rawDecodedData = json_decode($jsonData, true);
+    return $rawDecodedData['items'];
+}
--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'helpers.php';
 
 use GuzzleHttp\Client;
 
 $client = new Client();
-$date = new DateTime();
-$date = $date->sub(new DateInterval('P30D'));
-$date = $date->format('Y-m-d');
-$uri = 'https://api.github.com/search/repositories?q=created:>' . $date . '&sort=stars&order=desc&per_page=100';
+$uri = formatURI();
 $response = $client->request('GET', $uri);
-$rawDecodedData = json_decode($response->getBody(), true);
-$repositories = $rawDecodedData['items'];
+$repositories = decodeAndRemoveMetadata($response->getBody());
+
 echo '<pre>';
 print_r($repositories);
 echo '</pre>';

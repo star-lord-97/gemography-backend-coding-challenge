5fdb540663ec62241216fbad0c43a3904c3e705f
star-lord-97 <a_mahmoud_1997@yahoo.com>
2021-04-03 19:54:47 +0200

    added a route to GET the json representation of the languages and list of repos

--- routes.php
+++ routes.php
@@ -12,7 +12,16 @@ use function GuzzleHttp\json_encode;
 $router = new Router();
 
 $router->get('/gemography-backend-coding-challenge/github', function (ServerRequestInterface $request): ResponseInterface {
-    $response = new Response(200, ['Content-Type' => 'application/json'], json_encode(array('name' => 'Ahmed')));
+    $arrayToReturn = array();
+    foreach (collectLanguagesLinks() as $language => $links) {
+        array_push($arrayToReturn, array(
+            'language' => $language,
+            'number_of_repos' => count($links),
+            'list_of_repos' => $links,
+        ));
+    }
+
+    $response = new Response(200, ['Content-Type' => 'application/json'], json_encode($arrayToReturn));
     return $response;
 });
 
